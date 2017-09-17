cf9e393d6a75e9889c406bde0aa34c1d2e576787
Apple <apple@VMP-C02QQ8JVFVH7.local>
2017-09-16 15:09:44 -1000

    Add new iPhone 8, Apple TV, Watch Series 3

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ files.forEach((file) => {
   const data = fs.readFileSync(`./src/${file}`, 'utf8')
   const json = JSON.parse(data)
 
+  // Removes extra IDs in collections
+  if (json.collections) {
+    const collections = json.collections
+    const allLabelIds = json.labels.map(label => label.id)
+
+    collections.forEach((coll, index) => {
+      const collIds = coll.label_ids
+      const includedIds = collIds.filter(id => !allLabelIds.includes(id))
+
+      json.collections[index].label_ids = includedIds
+    })
+  }
+
   collections = collections.concat(json.collections)
   labels = labels.concat(json.labels)
 })
