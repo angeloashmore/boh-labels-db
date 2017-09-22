8b35d20c649f6f6ef69cacc856bc33892593b6c2
Apple <apple@VMP-C02QQ8JVFVH7.local>
2017-09-21 20:23:28 -1000

    Set new UUIDs for all collections

--- new_uuids.js
+++ new_uuids.js
@@ -3,25 +3,35 @@ import path from 'path'
 import uuid from 'node-uuid'
 import json from 'format-json'
 
-const filepath = path.join(process.cwd(), process.argv[2])
-const file = require(filepath, { encoding: 'utf-8' })
+const newUUIDs = filepath => {
+  const file = require(filepath, { encoding: 'utf-8' })
 
-const uuids = []
+  const uuids = []
 
-file.labels = file.labels.map(label => {
-  const id = uuid.v4()
+  file.labels = file.labels.map(label => {
+    const id = uuid.v4()
 
-  label.id = id
-  uuids.push(id)
+    label.id = id
+    uuids.push(id)
 
-  return label
-})
+    return label
+  })
 
-file.collections = file.collections.map(collection => {
-  collection.id = uuid.v4()
-  collection.label_ids = uuids
+  file.collections = file.collections.map(collection => {
+    collection.id = uuid.v4()
+    collection.label_ids = uuids
 
-  return collection
-})
+    return collection
+  })
 
-fs.writeFileSync(filepath, json.plain(file))
+  fs.writeFileSync(filepath, json.plain(file))
+}
+
+if (process.argv[2] === 'all') {
+  const dir = path.join(process.cwd(), 'src')
+  const filepaths = fs.readdirSync(dir)
+  filepaths.forEach(filepath => newUUIDs(path.join(process.cwd(), 'src', filepath)))
+} else {
+  const filepath = path.join(process.cwd(), process.argv[2])
+  newUUIDs(filepath)
+}
